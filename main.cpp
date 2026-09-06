5995d75b5c634843ef5d1c2b787d9e87fec1860f
samine825 <infilaxy@gmail.com>
2026-09-06 21:25:22 +0300

    делаю систему аргументов

--- main.cpp
+++ main.cpp
@@ -86,7 +86,6 @@ std::vector<std::wstring> generate_alphabets(std::vector<u64> seeds, int &lensee
 std::vector<std::wstring> seeds_req() {
 	std::wstring raw_seeds;
 	std::wcout << L"enter seeds: ";
-	std::wcin.ignore();
 	std::getline(std::wcin, raw_seeds);
 	
 	std::vector<u64> seeds;
@@ -117,10 +116,7 @@ std::wstring text_req() {
 
 
 //кодеры
-std::wstring encode() {
-	std::vector<std::wstring> abcs = seeds_req();
-	std::wstring text = text_req();
-	
+std::wstring encode(std::wstring text,std::vector<std::wstring> abcs) {
 	std::vector<wchar_t> arr(text.begin(), text.end());
 	std::wstring result = L"";
 	for (size_t i = 0; i<arr.size(); i++) {
@@ -128,10 +124,7 @@ std::wstring encode() {
     }
     return result;
 }
-std::wstring decode() {
-	std::vector<std::wstring> abcs = seeds_req();
-	std::wstring text = text_req();
-	
+std::wstring decode(std::wstring text,std::vector<std::wstring> abcs) {
 	std::vector<wchar_t> arr(text.begin(), text.end());
 	std::wstring result = L"";
 	std::wstring buffer = L"";
@@ -145,7 +138,14 @@ std::wstring decode() {
     return result;
 }
 
-
+int check(int argc, char* argv[], std::string_view target) {
+    for (int i = 1; i < argc; ++i) {
+        if (std::string_view(argv[i]) == target) {
+            return i;
+        }
+    }
+    return 0;
+}
 
 enum Options {
 	OptEncode,
@@ -158,54 +158,126 @@ int main(int argc, char* argv[]) {
 	std::setlocale(LC_ALL, "");
     std::wcout << std::format(L"HMcrypt {}\n", VERSION_STR/*при сборке, симейк ставит эту переменную*/) << std::endl;
     
-    
-    std::string arg1 = argv[1]; 
-    //std::string arg1 = argv[1]; 
-    //std::string arg1 = argv[1]; 
-    
     int selected_option = -1;
-    if (arg1 == "encode") {
-    	selected_option = Options::OptEncode;
+    try {
+        // проверка на аргумент режима
+        if (argc > 1) {
+            std::string arg1 = argv[1]; 
+            if (arg1 == "encode") {
+                selected_option = Options::OptEncode;
+            }
+            else if (arg1 == "decode") {
+                selected_option = Options::OptDecode;
+            }
+            else if (arg1 == "gencrt") {
+                selected_option = Options::OptGenCrt;
+            } 
+            else if (arg1 == "--help" || arg1 == "-h") {
+                std::wcout << "help menu" << std::endl;
+                return 0;
+            }
+            else {
+                throw std::invalid_argument("invalid first argument"); 
+            }
+        
+            std::string_view seed = "--seed";
+            std::string_view crt = "--crt";
+            std::string_view text = "--text";
+            std::string_view input = "--input";
+            std::string_view output = "--output";
+            std::string_view count = "--count";
+
+            int check_seed = check(argc, argv, seed);
+            int check_crt = check(argc, argv, crt);
+            int check_text = check(argc, argv, text);
+            int check_input = check(argc, argv, input);
+            int check_output = check(argc, argv, output);
+            int check_count = check(argc, argv, count);
+
+            if ((check_seed && check_crt) || (check_text && check_input) || (arg1 == "gencrt" && (check_crt || check_text || check_seed || check_input)) || ((arg1 == "decode" || arg1 == "encode") && check_count)) {
+                throw std::invalid_argument("invalid arguments"); 
+            }
+
+            
+            // сиды
+            if (check_crt) {
+                // сиды из сертификата
+            }
+            else if (check_seed) {
+                // сиды текстом
+            }
+            else {
+                //cli список сидов
+            }
+
+            // ввод
+            if (check_text) {
+                // текст
+            }
+            else if (check_input) {
+                // файл
+            }
+            else {
+                // cli ввод текста
+            }
+
+            // вывод (для всех режимов)
+            if (check_output) {
+                // вывод в файл
+            }
+            else {
+                // вывод к консоль
+            }
+
+
+            if (check_count) {
+                // количество сидов сертификата
+            }
+            else {
+                // базовое число, к примеру 1000
+            }
+        }
+        
+        //source aibrine-venv/bin/ctivate && python aibrine.py
+        // qs -p /home/samine/.config/quickshell/825UI/Shell.qml
+        
+        // основной цикл
+        bool running = true;
+        while (running) {
+            if (argc == 1) {
+                std::wcout << L"0. encode text\n1. decode text\n2. generate certificate\n3. exit" << std::endl;
+                std::wcout << L"option: ";
+                std::wstring temp_option;
+                std::getline(std::wcin, temp_option);
+                selected_option = std::stoi(temp_option);
+            }
+            switch (selected_option) {
+                case Options::OptEncode: {
+                    std::vector<std::wstring> abcs = seeds_req();
+                    std::wstring text = text_req();
+                    std::wstring encoded = encode(text, abcs);
+                    std::wcout << L"encoded text: " << encoded << std::endl;
+                    break;
+                }
+                case Options::OptDecode: {
+                    std::vector<std::wstring> abcs = seeds_req();
+                    std::wstring text = text_req();
+                    std::wstring decoded = decode(text, abcs);
+                    std::wcout << L"decoded text: " << decoded << std::endl;
+                    break;
+                }
+                case Options::OptExit:
+                    running = false;
+                    break;
+                default:
+                    running = false;
+                    std::wcout << L"invalid option" << std::endl;
+                    break;
+            }
+        }
     }
-    else if (arg1 == "decode") {
-    	selected_option = Options::OptDecode;
-    }
-    else if (arg1 == "gencrt") {
-    	selected_option = Options::OptGenCrt;
-    } else {
-    	selected_option = Options::OptExit;
-    }
-    
-    
-    
-    
-    // основной цикл
-    bool running = true; 
-    while (running) {
-	    if (selected_option == -1) {
-		    std::wcout << L"0. encode text\n1. decode text\n2. generate certificate\n3. exit" << std::endl;
-	        std::wcout << L"option: ";
-	        std::wcin >> selected_option;
-	    }
-	    switch (selected_option) {
-	    	case Options::OptEncode: {
-	            std::wstring encoded = encode();
-	            std::wcout << L"encoded text: " << encoded << std::endl;
-	            break;
-	        }
-	    	case Options::OptDecode: {
-	            std::wstring decoded = decode();
-	            std::wcout << L"decoded text: " << decoded << std::endl;
-	            break;
-	        }
-	        case Options::OptExit:
-            	running = false;
-	            break;
-	        default:
-            	running = false;
-	            std::wcout << L"invalid option" << std::endl;
-	            break;
-	    }
+    catch (const std::invalid_argument& e) { 
+        std::cerr << "Error: " << e.what() << "\n";
     }
     return 0;
 }
@@ -223,18 +295,34 @@ TODO: сделать обработку нечетных вводов (деко�
    ----------------------------
 encode  encode              gencrt
    --------                   |
-      |                       |
-   ------------------         |
- text   textfile   file   "f.825crt"
-   |      |         |         |
- "abc"  "f.txt"   "f.txt"    1000
-   |      |         |         |
-   |    "f.txt"   "f.825"    1000
-   ------------------
       |
    ---------
   seed    crt
    |       |
 "123 12" "f.825crt"
+   ---------
+      |                       |
+   ------------------         |
+ text   textfile   file   "f.825crt"
+   |      |         |         |
+ "abc"  "f.txt"   "f.any"    1000
+   ||      ||         |       |
+ "f.txt" "f.txt"  "f.825"    1000
+
+
+
+ hmcrypt [encode|decode] [seed|crt] [<seed>] [text|textfile|file] [<input>] [<output>]
+	
+ hmcrypt gencrt [<seeds count>] [<seeds range>]
+
+
+hmcrypt encode  --seed "123 12"          --text "hello world"
+hmcrypt encode  --crt  cert.crt          --input  data.bin   --output data.bin.enc
+hmcrypt encode  --crt  cert.crt          --input  notes.txt  --output notes.txt.enc
+hmcrypt decode  --seed "123 12"          --text "0a1b2c3d"
+hmcrypt decode  --crt  cert.crt          --input  data.bin.enc
+
+hmcrypt gencrt  --count 1000             [--range 0-999]     [--output cert.crt]
+
 
 */
